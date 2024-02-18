99164b786ed77b13051009fa6df673e2f17a68a1
Jeremy Twidt <jtwidt@gmail.com>
2024-02-17 21:20:21 -0600

    image details, update and delete functionality

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -226,6 +226,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
-import {CldImage} from "next-cloudinary";
-import {dataUrl, debounce, getImageSize} from "@/lib/utils";
+import {CldImage, getCldImageUrl} from "next-cloudinary";
+import {dataUrl, debounce, download, getImageSize} from "@/lib/utils";
 import {PlaceholderValue} from "next/dist/shared/lib/get-img-props";
 
 const TransformedImage = ({
@@ -13,7 +15,21 @@ const TransformedImage = ({
   transformationConfig,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -49,7 +65,7 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
@@ -58,10 +74,11 @@ const TransformedImage = ({
             <div className="transforming-loader">
               <Image
                 src="/assets/icons/spinner.svg"
-                alt="Transforming"
+                alt="spinner"
                 width={50}
                 height={50}
               />
+              <p className="text-white/80">Please wait...</p>
             </div>
           )}
         </div>
